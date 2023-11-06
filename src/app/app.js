ff03690c588371709c6c67e66fa728dffd5bc4db
Jose Germán Martínez <jgermanmm@gmail.com>
2023-11-06 19:08:35 -0300

    Add 404 error handler to API routes

--- src/app/app.js
+++ src/app/app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use("/api/v1", router );
+app.use('*', (req, res) => res.send("404 - Ruta no encontrada"))
 
 module.exports = app;
 
